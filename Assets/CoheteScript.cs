58145a045fbb9c29ad714672fb26374cc5e04677
gonzareyb <br0004@est.ort.edu.ar>
2025-03-27 12:01:01 -0300

    cohete fuego
    animamos el cohete y el fuego

---
+++ Assets/CoheteScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoheteScript : MonoBehaviour
+{
+
+    float coheteSpeed = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, coheteSpeed, 0);
+    }
+
+
+    public void Despegue()
+    {
+        coheteSpeed = 0.1f;
+        //debe activarse el objeto coheteFuego
+       
+    }
+}
