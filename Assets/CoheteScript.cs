c5e767f5f12bc2daf99d8a97402a9fd37877b52d
gonzareyb <br0004@est.ort.edu.ar>
2025-03-27 12:05:59 -0300

    Update CoheteScript.cs

--- Assets/CoheteScript.cs
+++ Assets/CoheteScript.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 
 public class CoheteScript : MonoBehaviour
 {
-<<<<<<< HEAD
 
     float coheteSpeed = 0;
-=======
+
     float coheteSpeed = 0;
 
->>>>>>> 259e3fa4dd151ee468f6aa7f53f1ed24ff7ff156
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,17 +22,17 @@ public class CoheteScript : MonoBehaviour
         transform.Translate(0, coheteSpeed, 0);
     }
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 259e3fa4dd151ee468f6aa7f53f1ed24ff7ff156
+
+
+
     public void Despegue()
     {
         coheteSpeed = 0.1f;
         //debe activarse el objeto coheteFuego
-<<<<<<< HEAD
+
        
-=======
->>>>>>> 259e3fa4dd151ee468f6aa7f53f1ed24ff7ff156
+
+
     }
 }
